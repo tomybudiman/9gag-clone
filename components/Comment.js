dda94e4d367ad6fbb0910d7f23c72855e207cf45
Rizky Agung Saputro <co.terbang@gmail.com>
2018-01-08 21:08:00 +0700

    Pull sebelum ngerjain

---
+++ components/Comment.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import {
+  View,
+  Text
+} from 'react-native'
+
+class Comment extends Component {
+  render(){
+    return (
+      <Text>comment component</Text>
+    )
+  }
+}
+
+export default Comment
